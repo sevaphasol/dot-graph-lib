5fa955329a9cce41283f1f840bf001570d36f3a1
Chocolate <artemnes1j@gmail.com>
2026-03-14 00:25:37 +0300

    Moved html to separate files, added methods for every attribute

---
+++ dot_graph/html.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "html_attributed.h"
+
+namespace html
+{
+
+class HTMLCell final : public HTMLAttributed<HTMLCell> {
+  private:
+    std::string placeholder_{};
+
+  public:
+    explicit HTMLCell( std::string placeholder);
+
+    explicit
+    operator std::string() const;
+
+  //
+  // Html attributes only for cells
+  //
+  public:
+    HTMLCell&
+    setStyle( std::string value ) &;
+
+    HTMLCell&
+    setColSpan( uint16_t value ) &;
+
+    HTMLCell&
+    setRowSpan( uint16_t value ) &;
+};
+
+class HTMLRow final {
+  private:
+    std::vector<HTMLCell> cells_{};
+
+  public:
+    explicit
+    operator std::string() const;
+
+    HTMLCell&
+    addCell( std::string placeholder) &;
+};
+
+class HTMLTable final : public HTMLAttributed<HTMLCell> {
+  private:
+    std::vector<HTMLRow> rows_{};
+
+  public:
+    explicit
+    operator std::string() const;
+
+    HTMLRow&
+    addRow() &;
+
+  //
+  // Html attributes only for tables
+  //
+  public:
+    HTMLTable&
+    setColumns( std::string value ) &;
+
+    HTMLTable&
+    setRows( std::string value ) &;
+
+    HTMLTable&
+    setStyle( std::string value ) &;
+};
+
+} // namespace html
+
+#include "dot_graph/html.inl"
